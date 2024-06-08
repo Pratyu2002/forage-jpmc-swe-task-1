10ae155886b2c4900a57e09daae2e9528c751615
Pratyu2002 <160417944+Pratyu2002@users.noreply.github.com>
2024-06-08 23:34:59 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -42,6 +42,9 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
+     if (price_b == 0):
+        #when price_b is 0 avoid throwing ZeroDivisionError
+       return 
     return 1
 
 
@@ -52,8 +55,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
